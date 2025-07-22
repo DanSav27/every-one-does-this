62481a9fa2d6f92cf741cb65faf916aeb34a844d
Sav2505 <sasa25051995@gmail.com>
2025-07-23 02:00:46 +0300

    bla

---
+++ Backend/src/db/db.ts
@@ -0,0 +1,40 @@
+import { Client } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// const dbClient = new Client({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: parseInt(process.env.DB_PORT || "5432"),
+// });
+
+const dbClient = new Client({
+  connectionString: process.env.DATABASE_URL,
+    ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+const connectToDB = async () => {
+  try {
+    await dbClient.connect();
+    console.log(`✅ Connected to PostgreSQL database on port: ${dbClient.port}`);
+  } catch (err) {
+    console.error("❌ Error connecting to the database:", err);
+    process.exit(1);
+  }
+};
+
+const disconnectFromDB = async () => {
+  try {
+    await dbClient.end();
+    console.log("✅ Disconnected from PostgreSQL database.");
+  } catch (err) {
+    console.error("❌ Error disconnecting from the database:", err);
+  }
+};
+
+export { dbClient, connectToDB, disconnectFromDB };
